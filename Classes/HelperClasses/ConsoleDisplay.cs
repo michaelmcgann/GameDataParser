46863cd80247a433d72ee97371f8b3a14310780f
Mike <126602126+michaelmcgann@users.noreply.github.com>
2024-10-02 12:34:42 +0100

    ..

--- Classes/HelperClasses/ConsoleDisplay.cs
+++ Classes/HelperClasses/ConsoleDisplay.cs
@@ -2,7 +2,7 @@ using Exceptions.Interfaces;
 
 namespace Exceptions.Classes.HelperClasses;
 
-public class Display : IDisplay {
+public class ConsoleDisplay : IDisplay {
 
     public void DisplayMessage(string message) {
         Console.WriteLine(message);
